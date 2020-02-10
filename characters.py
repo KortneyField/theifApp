223caa1da689fe71ea63dbe5f2639d85ba205e3a
Kortney <kfiel48@wgu.edu>
2020-02-09 23:05:09 -0600

    test commit

---
+++ characters.py
@@ -0,0 +1,5 @@
+class Character:
+    def __init__(self, name, **kwargs):
+        self.name = name
+        for key, value in kwargs.items():
+            setattr(self, key, value)
---
+++ play.py
@@ -0,0 +1,6 @@
+from thieves import Thief
+
+kortney = Thief("kortney", sneaky = False)
+print(kortney.sneaky)
+print(kortney.agile)
+print(kortney.hide(8))
